0792c7fd363c8a54959587bbc27e2563442f6f64
Harshit Agrawal <harshit.agrawal@adypu.edu.in>
2025-03-29 23:01:57 +0530

    final

--- game.js
+++ game.js
@@ -26,8 +26,6 @@ const checkWin = ()=>{
             document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
             isgameover = true
             document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
-            // document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
-            // document.querySelector(".line").style.width = "20vw";
             gameover.play();
         }
     })
